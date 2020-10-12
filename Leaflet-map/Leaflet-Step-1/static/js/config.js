cd19f0b7b0d964c6083029997bc46d7c7aae4a86
Lisa Weinstein <55967990+lisaweinst@users.noreply.github.com>
2020-10-12 18:58:29 -0500

    added leaflet map

---
+++ Leaflet-map/Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibGlzYXdlaW5zdCIsImEiOiJja2ZuNnI0aXUxcnJ1MnFwazFydmJpOHlzIn0.znZv3g3DCJ9s0NfWBpq2Kw";
--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask, jsonify, render_template
-from flask_cors import CORS, cross_origin
+#from flask_cors import CORS, cross_origin
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime;
 
+engine = create_engine('postgresql://postgres:Hershey1/@localhost:5432/RegionalJobMarket')
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+#Combined = Base.classes.combined 
+session = Session(engine)
+
+app = Flask(__name__)
+#cors = CORS(app)
+app.config['CORS_HEADERS'] = 'postgresql://postgres:Hershey1/@localhost:5432/RegionalJobMarket'
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+@app.route("/keepalive")
+def keep_alive():
+    return datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
